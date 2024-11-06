614176e3d85b86feb2a73189892940a0bd524ae2
Joy-mwende63 <mwendejoy47@gmail.com>
2024-11-06 16:03:39 +0300

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
-}
+function About()  {
+  return (
+    <div id="about">
+     <h2> About Me</h2>
+     <p>I am a passionate web developer with a focus on building dynamic and responsive websites. I enjoy learning new technologies and improving my skills.
+          </p>
+       <img src={image} alt="I made this" />
+
+    </div>
+  );
+};
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+// import React from "react";
+// import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
-}
+// function Home() {
+//   // update the JSX being returned!
+//   return <div>Home</div>;
+// }
+
+// export default Home;
+
+import React from 'react';
+
+// Assuming you have variables `name` and `city` defined in your Home.js
+const name = 'Liza';  // Replace with your actual name
+const city = 'New York';  // Replace with your actual city
+
+const Home = () => {
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
+};
 
 export default Home;
+
